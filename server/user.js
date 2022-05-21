3314a966cd307cd973bd35ce3551867d7fa2335a
maruyu998 <maruyu998@gmail.com>
2022-05-21 17:20:26 +0900

    support for webpush and for getting friends' sum

--- server/user.js
+++ server/user.js
@@ -1,5 +1,6 @@
 const User = require('./db/user');
 const Friend = require('./db/friend');
+const webpush = require('./webpush');
 
 module.exports.getSummary = async (req, res) => {
     const username = req.session.username;
@@ -14,10 +15,24 @@ module.exports.getSummary = async (req, res) => {
 
 module.exports.getUserFriends = async (req, res) => {
     const username = req.session.username;
+    const amount_dict = {};
     const friends = await Promise.all((await Friend.find({username}).exec()).map(async (f) => {
-        if(!f.accepted) return { username: f.friendname, accepted: f.accepted, amount: null }
+        if(!f.accepted) return { username: f.friendname, accepted: f.accepted, amount: null, friendsamount:null }
         const user = await User.findOne({username: f.friendname}).exec();
-        return { username: f.friendname, accepted: f.accepted, amount: user.amount }
+        amount_dict[user.username] = user.amount;
+        const _friends = await Friend.find({username, accepted:true}).exec();
+        let friendsamount = 0;
+        for(let f of _friends){
+            let user_amount = 0;
+            if(amount_dict[f.friendname] === undefined){
+                user_amount = (await User.findOne({username: f.friendname})).amount;
+                amount_dict[f.friendname] = user_amount
+            } else {
+                user_amount = amount_dict[f.friendname]
+            }
+            friendsamount += user_amount
+        }
+        return { username: f.friendname, accepted: f.accepted, amount: user.amount, friendsamount }
     }));
     const requested_friends = (await Friend.find({friendname: username, accepted: false}).exec()).map(f => (
         { username: f.username, accepted: false, amount: null }
@@ -47,16 +62,18 @@ module.exports.requestAddFriend = async (req, res) => {
         })        
         return
     }
-    await Friend({username, friendname, accepted:false}).save()
+    const friend = await Friend({username, friendname, accepted:false}).save()
     res.json({
         messages: [{type: 'info', text: 'requested successfully.'}]
     })
+    webpush.sendRequestForAcceptFriend(friend);
 }
 
 module.exports.acceptFriend = async (req, res) => {
     const username = req.session.username;
     const friendname = req.body.friendname;
-    if(!await Friend.findOne({username: friendname, friendname: username}).exec()){
+    const friend = await Friend.findOne({username: friendname, friendname: username}).exec();
+    if(!friend){
         res.json({
             messages: [{type:'warning', text: 'invalid acception.'}]
         })
@@ -74,4 +91,5 @@ module.exports.acceptFriend = async (req, res) => {
     res.json({
         messages: [{type: 'info', text: 'accepted successfully.'}]
     })
+    webpush.sendMessageForAcceptFriend(friend);
 }
