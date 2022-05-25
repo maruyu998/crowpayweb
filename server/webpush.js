79369491ec3aa06f012ca7b48b90d344d72b9959
maruyu998 <maruyu998@gmail.com>
2022-05-25 16:57:20 +0900

    using mongoose to subscrib and modify cjs to es6

--- server/webpush.js
+++ server/webpush.js
@@ -1,5 +1,6 @@
-const config = require('config');
-const webpush = require('web-push');
+import config from 'config';
+import webpush from 'web-push';
+import mongoose from 'mongoose';
 
 webpush.setVapidDetails(
     'mailto:yukimaru@maruyu.work',
@@ -7,102 +8,104 @@ webpush.setVapidDetails(
     config.webpush_private_key
 );
 
-const subscriptions = {};
+const Subscription = mongoose.model('subscription', 
+    new mongoose.Schema({
+        username: String,
+        endpoint: String,
+        subscription: Object
+    })
+)
 
-module.exports.getPublicKey = (req, res) => {
-    res.json({publicKey : config.webpush_public_key});
-}
-module.exports.addSubscription = (req, res) => {
-    const username = req.session.username;
-    if(subscriptions[username] === undefined) subscriptions[username] = {}
-    const subscription = req.body.subscription
-    subscriptions[username][subscription.endpoint] = subscription
-    res.send('ok')
-}
-
-module.exports.sendRequestForAcceptTransaction = (transaction) => {
-    const w = (transaction.accepter == transaction.receiver) ? "支払い" : "請求";
-    const object = {
-        title: `取引承認依頼(${transaction.content})`,
-        body: `${transaction.issuer}より¥ ${transaction.amount}の${w}が届いています．`,
-        actions: [{action:'openTransaction', title:'取引を確認する'}]
+export default class {
+    static getPublicKey = (req, res) => {
+        res.json({publicKey : config.webpush_public_key});
     }
-    for(let subscription of Object.values(subscriptions[transaction.accepter] || {})){
-        try{
-            webpush.sendNotification(subscription, JSON.stringify(object));
-        } catch(e){console.error(e)}
+    static addSubscription = async (req, res) => {
+        const username = req.session.username;
+        const subscription = req.body.subscription
+        if(!await Subscription.findOne({username, endpoint:subscription.endpoint})){
+            await Subscription({username, endpoint:subscription.endpoint, subscription}).save();
+        }
+        res.send('ok');
     }
-}
-
-module.exports.sendRequestForAcceptTransaction = (transaction) => {
-    const w = (transaction.accepter == transaction.receiver) ? "支払い" : "請求";
-    const object = {
-        tag: 'crowpay-issue-transaction',
-        title: `取引承認依頼(${transaction.content})`,
-        body: `${transaction.issuer}より¥ ${transaction.amount}の${w}が届いています．`,
-        actions: [{action:'openTransaction', title:'取引を確認する'}]
+    
+    static sendRequestForAcceptTransaction = async (transaction) => {
+        const w = (transaction.accepter == transaction.receiver) ? "支払い" : "請求";
+        const object = {
+            tag: 'crowpay-issue-transaction',
+            title: `取引承認依頼(${transaction.content})`,
+            body: `${transaction.issuer}より¥ ${transaction.amount}の${w}が届いています．`,
+            actions: [{action:'openTransaction', title:'取引を確認する'}]
+        }
+        
+        const subscriptions = (await Subscription.find({username:transaction.accepter}).exec()).map(s=>s.subscription);
+        console.log(subscriptions)
+        for(let subscription of subscriptions){
+            try{
+                webpush.sendNotification(subscription, JSON.stringify(object));
+            } catch(e){console.error(e)}
+        }
     }
-    for(let subscription of Object.values(subscriptions[transaction.accepter] || {})){
-        try{
-            webpush.sendNotification(subscription, JSON.stringify(object));
-        } catch(e){console.error(e)}
+    
+    static sendMessageForAcceptTransaction = async (transaction) => {
+        const w = (transaction.accepter == transaction.receiver) ? "支払い" : "請求";
+        const object = {
+            tag: 'crowpay-accept-transaction',
+            title: `取引が承認されました(${transaction.content})`,
+            body: `${transaction.accepter}より¥ ${transaction.amount}の${w}が承認されました．`,
+            actions: [{action:'openTransaction', title:'取引を承認・確認する'}]
+        }
+        const subscriptions = (await Subscription.find({username:transaction.issuer}).exec()).map(s=>s.subscription);
+        for(let subscription of subscriptions){
+            try{
+                webpush.sendNotification(subscription, JSON.stringify(object));
+            } catch(e){console.error(e)}
+        }
     }
-}
-
-module.exports.sendMessageForAcceptTransaction = (transaction) => {
-    const w = (transaction.accepter == transaction.receiver) ? "支払い" : "請求";
-    const object = {
-        tag: 'crowpay-accept-transaction',
-        title: `取引が承認されました(${transaction.content})`,
-        body: `${transaction.accepter}より¥ ${transaction.amount}の${w}が承認されました．`,
-        actions: [{action:'openTransaction', title:'取引を承認・確認する'}]
+    
+    static sendMessageForDeclineTransaction = async (transaction) => {
+        const w = (transaction.accepter == transaction.receiver) ? "支払い" : "請求";
+        const object = {
+            tag: 'crowpay-decline-transaction',
+            title: `取引が却下されました(${transaction.content})`,
+            body: `${transaction.accepter}より¥ ${transaction.amount}の${w}が却下されました．`,
+            actions: [{action:'openTransaction', title:'取引を確認する'}]
+        }
+        const subscriptions = (await Subscription.find({username:transaction.issuer}).exec()).map(s=>s.subscription);
+        for(let subscription of subscriptions){
+            try{
+                webpush.sendNotification(subscription, JSON.stringify(object));
+            } catch(e){console.error(e)}
+        }
     }
-    for(let subscription of Object.values(subscriptions[transaction.issuer] || {})){
-        try{
-            webpush.sendNotification(subscription, JSON.stringify(object));
-        } catch(e){console.error(e)}
+    
+    static sendRequestForAcceptFriend = async (friend) => {
+        const object = {
+            tag: 'crowpay-issue-friend',
+            title: `友人申請(${friend.username})`,
+            body: `${friend.username}より友人申請が届いています`,
+            actions: [{action:'openUser', title:'承認・確認する'}]
+        }
+        const subscriptions = (await Subscription.find({username:friend.friendname}).exec()).map(s=>s.subscription);
+        for(let subscription of subscriptions){
+            try{
+                webpush.sendNotification(subscription, JSON.stringify(object));
+            } catch(e){console.error(e)}
+        }
     }
-}
-
-module.exports.sendMessageForDeclineTransaction = (transaction) => {
-    const w = (transaction.accepter == transaction.receiver) ? "支払い" : "請求";
-    const object = {
-        tag: 'crowpay-decline-transaction',
-        title: `取引が却下されました(${transaction.content})`,
-        body: `${transaction.accepter}より¥ ${transaction.amount}の${w}が却下されました．`,
-        actions: [{action:'openTransaction', title:'取引を確認する'}]
-    }
-    for(let subscription of Object.values(subscriptions[transaction.issuer] || {})){
-        try{
-            webpush.sendNotification(subscription, JSON.stringify(object));
-        } catch(e){console.error(e)}
-    }
-}
-
-module.exports.sendRequestForAcceptFriend = (friend) => {
-    const object = {
-        tag: 'crowpay-issue-friend',
-        title: `友人申請(${friend.username})`,
-        body: `${friend.username}より友人申請が届いています`,
-        actions: [{action:'openUser', title:'承認・確認する'}]
-    }
-    for(let subscription of Object.values(subscriptions[friend.friendname] || {})){
-        try{
-            webpush.sendNotification(subscription, JSON.stringify(object));
-        } catch(e){console.error(e)}
-    }
-}
-
-module.exports.sendMessageForAcceptFriend = (friend) => {
-    const object = {
-        tag: 'crowpay-issue-friend',
-        title: `友人申請承認(${friend.friendname})`,
-        body: `${friend.friendname}より友人申請が承認されました．`,
-        actions: [{action:'openUser', title:'承認・確認する'}]
-    }
-    for(let subscription of Object.values(subscriptions[friend.username] || {})){
-        try{
-            webpush.sendNotification(subscription, JSON.stringify(object));
-        } catch(e){console.error(e)}
+    
+    static sendMessageForAcceptFriend = async (friend) => {
+        const object = {
+            tag: 'crowpay-issue-friend',
+            title: `友人申請承認(${friend.friendname})`,
+            body: `${friend.friendname}より友人申請が承認されました．`,
+            actions: [{action:'openUser', title:'承認・確認する'}]
+        }
+        const subscriptions = (await Subscription.find({username:friend.username}).exec()).map(s=>s.subscription);
+        for(let subscription of subscriptions){
+            try{
+                webpush.sendNotification(subscription, JSON.stringify(object));
+            } catch(e){console.error(e)}
+        }
     }
 }
